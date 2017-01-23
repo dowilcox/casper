a88b9bdd1a7e564c09fbeeef6174e355b127c03c
Unknown <donwil92@me.com>
2017-01-22 22:30:44 -0500

    Removing side bar.

--- sidebar.php
+++ sidebar.php
@@ -5,9 +5,3 @@
  * @package Casper
  */
 ?>
-	<div id="secondary" class="widget-area" role="complementary">
-		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : 
-			// Put default widgets here
-		endif; // end sidebar widget area ?>
-		<div class="clear">&nbsp;</div>
-	</div><!-- #secondary -->
